d514a7d9a7856c85cbec44e35051467138295506
_meugen_ <eugen35@mail.ru>
2018-11-15 12:02:28 +0500

    Рабочий вариант. Пока тесты все в одном файле

--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 #include "mylibs/foo.h"
 using namespace std;
 
-// @todo /1/ Тестирование массива как сделать
-// @todo /1/ Вынести тесты в отдельный файл
-// @todo /1/ Запуск тестов тоже вынести в отдельный файл
-
 int main(int argc, char **argv) {
     cout << sum(1,2) << endl;
     return 0;
--- test_main.cpp
+++ test_main.cpp
@@ -9,6 +9,10 @@ TEST(SumTest, A_Add_Zero) {
     EXPECT_EQ (sum(0,2), 3);
 }
 
+
+// @todo /1/ Тестирование массива как сделать
+// @todo /3/ ОЧЕНЬ ДОЛГО НУЖНО КУРИТЬ КАК ВЫНЕСТИ ВСЕ ТЕСТЫ В ОТДЕЛЬНЫЙ КАТАЛОГ ИЛИ ХОТЯ БЫ В ОТДЕЛЬНЫЙ ФАЙЛ И ЗАПУСКАТЬ ИХ !!!!??? Поэтому пока откладываем
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
